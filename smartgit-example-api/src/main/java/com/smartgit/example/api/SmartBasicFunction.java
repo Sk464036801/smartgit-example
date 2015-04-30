a43e6275893b3c5c3480a239021bdd90cf739fdb
shangke <xiaoshang_ke@sina.cn>
2015-04-30 15:08:29 +0800

    local Repository master-devlop branch commit 1

--- smartgit-example-api/src/main/java/com/smartgit/example/api/SmartBasicFunction.java
+++ smartgit-example-api/src/main/java/com/smartgit/example/api/SmartBasicFunction.java
@@ -24,6 +24,8 @@ public class SmartBasicFunction {
 
         System.out.println("local Repository eidt, example multy branch rebase function. commit 3");
 
+        System.out.println("local Repository master-devlop branch commit 1.");
+
     }
 
 }
