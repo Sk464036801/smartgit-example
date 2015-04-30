7aa29abda86f2b6fe078bfcc4f3e644a3857367e
shangke <xiaoshang_ke@sina.cn>
2015-04-30 11:44:21 +0800

    smart git basic function example, commit local repository 1.

---
+++ smartgit-example-api/src/main/java/com/smartgit/example/api/SmartBasicFunction.java
@@ -0,0 +1,14 @@
+package com.smartgit.example.api;
+
+/**
+ * Created by ke on 2015/4/30.
+ */
+public class SmartBasicFunction {
+
+    public static void main(String[] args) {
+
+        System.out.println("Basic Function discard, remove, delete example.");
+
+    }
+
+}
